27ab5d0f01127fd3b46ddf2433afbf76c8424c12
Mark <mtstarbinski@outlook.com>
2022-01-03 20:27:34 -0500

    Updates

---
+++ precache-manifest.f04c3206826bab38ed35c9dbc5836781.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e67e8b4458adccc23a5423968de325c6",
+    "url": "/react-to-do-list/index.html"
+  },
+  {
+    "revision": "e5f2aec77137107e26d1",
+    "url": "/react-to-do-list/static/js/2.c475bd67.chunk.js"
+  },
+  {
+    "revision": "aca31ae645c5f093053b",
+    "url": "/react-to-do-list/static/js/main.e9053513.chunk.js"
+  },
+  {
+    "revision": "f39293437b175608c0b2",
+    "url": "/react-to-do-list/static/js/runtime-main.aa2643de.js"
+  }
+]);
